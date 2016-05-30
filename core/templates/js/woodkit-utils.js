a0506c70323faa0a4dbfd32f7a14403944d5c2b6
seb-montana <seb-montana@users.noreply.github.com>
2016-05-30 13:00:53 +0200

    - js-utils : add get_url_parameter(name)

--- core/templates/js/woodkit-utils.js
+++ core/templates/js/woodkit-utils.js
@@ -51,6 +51,22 @@ function indexOf(value, array) {
 }
 
 /**
+ * Get url parameter
+ */
+function get_url_parameter(param) {
+	var vars = {};
+	window.location.href.replace(location.hash, '').replace(/[?&]+([^=&]+)=?([^&]*)?/gi, // regexp
+	function(m, key, value) { // callback
+		vars[key] = value !== undefined ? value : '';
+	});
+
+	if (param) {
+		return vars[param] ? vars[param] : null;
+	}
+	return vars;
+}
+
+/**
  * Show wait box
  */
 function wait($element) {
