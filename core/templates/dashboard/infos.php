0fb1fc305125aedfaca7e6586f82c5e7a1e7ae85
seb-montana <seb-montana@users.noreply.github.com>
2016-05-30 13:58:06 +0200

    - dashboard : update translation text-domain

--- core/templates/dashboard/infos.php
+++ core/templates/dashboard/infos.php
@@ -25,9 +25,9 @@ defined('ABSPATH') or die("Go Away!");
 ?>
 
 <div class="woodkit-dashboard-widget">
-	<p><?php _e("Thanks for using woodkit."); ?></p>
-	<p><?php _e("Woodkit offers you lots of awesome tools to improve your experience on wordpress, on the Web. SEO, security, private site, social publication, ..."); ?></p>
-	<p><?php _e("You can discover and manage tools"); ?>&nbsp;<a href="<?php echo esc_url(get_admin_url(null, 'options-general.php?page=woodkit_options')); ?>"><?php _e("here"); ?></a>.</p>
-	<p><?php _e("Do you need some"); ?>&nbsp;<a href="<?php echo esc_url(WOODKIT_URL_DOCUMENTATION); ?>" target="_blank"><?php _e("documentation"); ?></a> ?</p>
+	<p><?php _e("Thanks for using woodkit.", WOODKIT_PLUGIN_TEXT_DOMAIN); ?></p>
+	<p><?php _e("Woodkit offers you lots of awesome tools to improve your experience on wordpress, on the Web. SEO, security, private site, social publication, ...", WOODKIT_PLUGIN_TEXT_DOMAIN); ?></p>
+	<p><?php _e("You can discover and manage tools", WOODKIT_PLUGIN_TEXT_DOMAIN); ?>&nbsp;<a href="<?php echo esc_url(get_admin_url(null, 'options-general.php?page=woodkit_options')); ?>"><?php _e("here", WOODKIT_PLUGIN_TEXT_DOMAIN); ?></a>.</p>
+	<p><?php _e("Do you need some", WOODKIT_PLUGIN_TEXT_DOMAIN); ?>&nbsp;<a href="<?php echo esc_url(WOODKIT_URL_DOCUMENTATION); ?>" target="_blank"><?php _e("documentation", WOODKIT_PLUGIN_TEXT_DOMAIN); ?></a> ?</p>
 	<p style="text-align: right;"><a href="<?php echo esc_url("http://www.studio-montana.com"); ?>" target="_blank">Studio Montana</a></p>
 </div>
