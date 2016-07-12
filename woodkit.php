49b2e1679509e76d6eedea748ffe832546c7cb72
seb-montana <seb-montana@users.noreply.github.com>
2016-07-12 10:36:49 +0200

    - add localstorage manager (clear local storage in WP_DEBUG mode - useful to develop Divi Modules)

--- woodkit.php
+++ woodkit.php
@@ -4,7 +4,7 @@
  * Plugin Name: Woodkit
  * Plugin URI: http://www.studio-montana.com/product/woodkit
  * Description: Multitool experience for WP | SEO, security, masonry, private site, social publication, ...
- * Version: 1.1.50
+ * Version: 1.1.51
  * Author: Studio Montana
  * Author URI: http://www.studio-montana.com/
  * License: GPL2
@@ -44,6 +44,7 @@ define('WOODKIT_PLUGIN_CORE_FOLDER', 'core/');
 define('WOODKIT_PLUGIN_COMMONS_FOLDER', 'core/commons/');
 define('WOODKIT_PLUGIN_CONFIG_FOLDER', 'core/commons/config/');
 define('WOODKIT_PLUGIN_DIVI_FOLDER', 'core/commons/divi/');
+define('WOODKIT_PLUGIN_LOCALSTORAGE_FOLDER', 'core/commons/localstorage/');
 define('WOODKIT_PLUGIN_INSTALLER_FOLDER', 'core/commons/installer/');
 define('WOODKIT_PLUGIN_TEMPLATES_FOLDER', 'core/templates/');
 define('WOODKIT_PLUGIN_TEMPLATES_DASHBOARD_FOLDER', 'core/templates/dashboard/');
@@ -95,6 +96,9 @@ if(!class_exists('Woodkit')){
 				
 			/** tools */
 			require_once (WOODKIT_PLUGIN_PATH.'/'.WOODKIT_PLUGIN_COMMONS_FOLDER.'tools.php');
+				
+			/** localstorage */
+			require_once (WOODKIT_PLUGIN_PATH.'/'.WOODKIT_PLUGIN_LOCALSTORAGE_FOLDER.'localstorage.php');
 				
 			/** divi */
 			require_once (WOODKIT_PLUGIN_PATH.'/'.WOODKIT_PLUGIN_DIVI_FOLDER.'divi.php');
