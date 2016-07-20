d014d01e44fcb06b961d44b4e3a234d30a906b14
seb-montana <seb-montana@users.noreply.github.com>
2016-07-20 11:26:32 +0200

    - increase version

--- woodkit.php
+++ woodkit.php
@@ -4,7 +4,7 @@
  * Plugin Name: Woodkit
  * Plugin URI: http://www.studio-montana.com/product/woodkit
  * Description: Multitool experience for WP | SEO, security, masonry, private site, social publication, ...
- * Version: 1.1.55
+ * Version: 1.1.56
  * Author: Studio Montana
  * Author URI: http://www.studio-montana.com/
  * License: GPL2
