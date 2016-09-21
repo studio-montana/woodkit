4c17c2f2d8bfe24b634bfdc392ef2562ab69ac7a
seb-montana <seb-montana@users.noreply.github.com>
2016-09-21 10:05:08 +0200

    wall - correct issue discover in 1.1.67

--- core/tools/wall/templates/tool-wall-display.php
+++ core/tools/wall/templates/tool-wall-display.php
@@ -95,15 +95,9 @@ if (post_type_exists($wall_args['meta_wall_display_post_type'])){ // dynamic lis
 			unset($ids_tab[$key]);
 		}
 		if (!empty($ids_tab)){
-			// Some problems using WP_Query		 
-			// $args = array("posts_per_page" => -1, "post_type" => "any", "post__in" => $ids_tab, "orderby" => "post__in", "suppress_filters" => FALSE, "post_status" => "any", "ignore_sticky_posts" => true);
-			// $get_any_posts = new WP_Query;
-			// $posts = $get_any_posts->query($args);
-			
-			// Use super ancestral method !
-			foreach ($ids_tab as $id){
-				$posts[] = get_post($id);
-			}
+			$args = array("posts_per_page" => -1, "post_type" => "any", "post__in" => $ids_tab, "orderby" => "post__in", "suppress_filters" => FALSE, "post_status" => "any", "ignore_sticky_posts" => true);
+			$get_any_posts = new WP_Query;
+			$posts = $get_any_posts->query($args);
 		}
 	}
 }
