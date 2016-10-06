1b40391681be404ffa3f6f653720f8e802a80774
seb-montana <seb-montana@users.noreply.github.com>
2016-10-06 11:17:58 +0200

    - tool googleanalytics : add event tracking function

--- core/tools/googleanalytics/googleanalytics.php
+++ core/tools/googleanalytics/googleanalytics.php
@@ -23,26 +23,35 @@
 defined('ABSPATH') or die("Go Away!");
 
 /**
- * WP_Footer hook
+ * WP_Head hook
  *
  * @since Woodkit 1.0
  * @return void
 */
-function tool_googleanalytics_wp_footer() {
+function tool_googleanalytics_wp_head() {
 	$googleanalytics_code = woodkit_get_option('tool-googleanalytics-code');
 	if (!empty($googleanalytics_code)){
 		?>
-<script type="text/javascript">
-				var _gaq = _gaq || [];
-				_gaq.push(['_setAccount', '<?php echo $googleanalytics_code; ?>']);
-				_gaq.push(['_trackPageview']);
-				(function() {
-					var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-					ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-					var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-				})();
-			</script>
+<!-- Google Analytics -->
+<script>
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+ga('create', '<?php echo $googleanalytics_code; ?>', 'auto');
+ga('send', 'pageview');
+
+/**
+ * Google Analitycs Event Tracking
+ */
+function woodkit_tool_googleanalytics_event_tracking(eventCategory, eventAction, eventLabel){
+	ga('send', 'event', eventCategory, eventAction, eventLabel);
+}
+
+</script>
+<!-- End Google Analytics -->
 <?php
 	}
 }
-add_action('wp_footer', 'tool_googleanalytics_wp_footer');
+add_action('wp_head', 'tool_googleanalytics_wp_head');
