7e850c840b24227280960e2a7522a101b1b29131
seb-montana <seb-montana@users.noreply.github.com>
2017-04-15 14:30:22 +0200

    - don't load map if no api key (Woodkit or Divi)

--- core/tools/googlemaps/widgets/templates/tool-googlemaps-widget-output.php
+++ core/tools/googlemaps/widgets/templates/tool-googlemaps-widget-output.php
@@ -30,13 +30,24 @@ defined('ABSPATH') or die("Go Away!");
 	<div style="overflow:hidden; width:<?php echo $gmapswidth; ?>; height:<?php echo $gmapsheight; ?>;">
 		<div id='<?php echo $gmapsid; ?>' class="googlemaps-canvas" style="width:<?php echo $gmapswidth; ?>; height:<?php echo $gmapsheight; ?>;"></div>
 	</div>
-	<script type="text/javascript">
-	jQuery(document).ready(function($){
-		google.maps.event.addDomListener(window, 'load', function(){
-			var map = new google.maps.Map(document.getElementById('<?php echo $gmapsid; ?>'), {zoom:<?php echo $gmapszoom; ?>, mapTypeId: <?php echo $gmapstype; ?>, zoomControl:<?php echo $gmapszoomcontrol; ?>, streetViewControl:<?php echo $gmapsstreetviewcontrol; ?>, scaleControl:<?php echo $gmapsscalecontrol; ?>, mapTypeControl:<?php echo $gmapsmaptypecontrol; ?>, rotateControl:<?php echo $gmapsrotatecontrol; ?>, scrollwheel:<?php echo $gmapsscrollwheel; ?>});
-			geocode_adress(map, new google.maps.Geocoder(), "<?php echo $gmapsaddress; ?>", "<?php echo $gmapstitle; ?>");
+	<?php 
+	$has_googlemaps_api = false;
+	if (function_exists("et_pb_get_google_api_key")){ // Divi API settings
+		$googlemap_api_key = et_pb_get_google_api_key();
+		$has_googlemaps_api = !empty($googlemap_api_key);
+	}else{
+		$googlemap_api_key = woodkit_get_option('tool-googlemaps-apikey', '');
+		$has_googlemaps_api = !empty($googlemap_api_key);
+	}
+	if ($has_googlemaps_api){
+		?>
+		<script type="text/javascript">
+		jQuery(document).ready(function($){
+			google.maps.event.addDomListener(window, 'load', function(){
+				var map = new google.maps.Map(document.getElementById('<?php echo $gmapsid; ?>'), {zoom:<?php echo $gmapszoom; ?>, mapTypeId: <?php echo $gmapstype; ?>, zoomControl:<?php echo $gmapszoomcontrol; ?>, streetViewControl:<?php echo $gmapsstreetviewcontrol; ?>, scaleControl:<?php echo $gmapsscalecontrol; ?>, mapTypeControl:<?php echo $gmapsmaptypecontrol; ?>, rotateControl:<?php echo $gmapsrotatecontrol; ?>, scrollwheel:<?php echo $gmapsscrollwheel; ?>});
+				geocode_adress(map, new google.maps.Geocoder(), "<?php echo $gmapsaddress; ?>", "<?php echo $gmapstitle; ?>");
+			});
 		});
-	});
-	</script>
-
+		</script>
+	<?php } ?>
 </div>
