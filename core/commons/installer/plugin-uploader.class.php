d91e3802a2970fe899697bef533bf3c60a90070a
seb-montana <seb-montana@users.noreply.github.com>
2018-03-19 10:09:25 +0100

    - prerelease : test upgrader

--- core/commons/installer/plugin-uploader.class.php
+++ core/commons/installer/plugin-uploader.class.php
@@ -217,6 +217,10 @@ class WoodkitPluginUploader {
 	 * Perform additional actions to successfully install our plugin
 	 */
 	public function postInstall($true, $hook_extra, $result) {
+		
+		trace_info("woodkit - postInstall");
+		trace_info("woodkit - postInstall - result : ".var_export($result, true));
+		
 		// Get plugin information
 		$this->initPluginData();
 
@@ -225,6 +229,8 @@ class WoodkitPluginUploader {
 
 			// Remember if our plugin was previously activated
 			$wasActivated = is_plugin_active($this->slug);
+			
+			trace_info("woodkit - postInstall - is_plugin_active : ".var_export($wasActivated, true));
 
 			// Since we are hosted in GitHub, our plugin folder would have a dirname of
 			// reponame-tagname change it to our original one:
@@ -232,6 +238,8 @@ class WoodkitPluginUploader {
 			$pluginFolder = WP_PLUGIN_DIR.DIRECTORY_SEPARATOR.dirname($this->slug);
 			$wp_filesystem->move( $result['destination'], $pluginFolder );
 			$result['destination'] = $pluginFolder;
+			
+			trace_info("woodkit - postInstall - destination : ".var_export($pluginFolder, true));
 
 			// Re-activate plugin if needed
 			if ($wasActivated) {
@@ -239,6 +247,8 @@ class WoodkitPluginUploader {
 			}
 			
 			$plugin_data = get_plugin_data($this->pluginFile);
+
+			trace_info("woodkit - postInstall - plugin_data : ".var_export($plugin_data, true));
 			
 			woodkit_after_auto_update($this->package, $plugin_data["Version"]);
 		}
